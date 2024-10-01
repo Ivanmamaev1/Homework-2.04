cac0ff3c2a81b7f334941d23acd506d987c05366
Ivanmamaev1 <mamaevivan92@gmail.com>
2024-10-01 12:06:52 +0300

    First project settings. Creating view interface. Programming logic.

--- Homework 2.04/ViewController.swift
+++ Homework 2.04/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var pointOfRedLabel: UILabel!
+    @IBOutlet var pointOfGreenLabel: UILabel!
+    @IBOutlet var pointOfBlueLabel: UILabel!
+    
+    @IBOutlet var changingColorView: UIView!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changingColorView.layer.cornerRadius = 25
+        
+        pointOfRedLabel.text = redSlider.value.formatted()
+        pointOfGreenLabel.text = greenSlider.value.formatted()
+        pointOfBlueLabel.text = blueSlider.value.formatted()
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func changeViewColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        changingColorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    @IBAction func redSliderAction() {
+            pointOfRedLabel.text = redSlider.value.formatted()
+    }
+    
+    @IBAction func greenSliderAction() {
+        pointOfGreenLabel.text = greenSlider.value.formatted()
+    }
+    
+    @IBAction func blueSliderAction() {
+        pointOfBlueLabel.text = blueSlider.value.formatted()
+    }
 }
-
